7043c03cc0615e79c9a99c5070a9836fbd9e11bf
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-14 14:36:24 +0530

    Implement User, UserProfile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(max_length=255, null=False, unique=True)
+    phone_number = models.CharField(max_length=10, unique=True)
+    is_active = models.BooleanField(default=False)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    DEFAULT_PROFILE_PIC_URL = "https://mywebsite.com/placeholder.png"
+
+    profile_pic_url = models.URLField(default=DEFAULT_PROFILE_PIC_URL)
+    bio = models.TextField(max_length=255, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.user.name
